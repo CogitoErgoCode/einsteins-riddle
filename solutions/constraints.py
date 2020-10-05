63cd3065a3033d9d1f2ad5a0671646d7de731491
CogitoErgoCode <sevenwolf@protonmail.com>
2020-10-05 00:18:35 -0700

    whitespace

--- solutions/constraints.py
+++ solutions/constraints.py
@@ -2,70 +2,70 @@ from constraint import *
 import pandas as pd
 
 def main():
-	problem = Problem()
+    problem = Problem()
 
-	variables = {
-		'houses' : ['red'     , 'green'  , 'white' , 'yellow'    , 'blue'  ],
-		'races'  : ['brit'    , 'swede'  , 'dane'  , 'norwegian' , 'german'],
-		'drinks' : ['tea'     , 'coffee' , 'milk'  , 'beer'      , 'water' ],
-		'smokes' : ['pallMall', 'dunhill', 'blends', 'blueMaster', 'prince'],
-		'pets'   : ['dogs'    , 'birds'  , 'cats'  , 'horses'    , 'fish'  ]
-	}
+    variables = {
+        'houses' : ['red'     , 'green'  , 'white' , 'yellow'    , 'blue'  ],
+        'races'  : ['brit'    , 'swede'  , 'dane'  , 'norwegian' , 'german'],
+        'drinks' : ['tea'     , 'coffee' , 'milk'  , 'beer'      , 'water' ],
+        'smokes' : ['pallMall', 'dunhill', 'blends', 'blueMaster', 'prince'],
+        'pets'   : ['dogs'    , 'birds'  , 'cats'  , 'horses'    , 'fish'  ]
+    }
 
-	# addVariables: each list gets numbered 1 - 5
-	# addConstraint: apply exclusivity
-	for values in variables.values():
-		problem.addVariables(values, range(1,5+1))
-		problem.addConstraint(AllDifferentConstraint(), values)
+    # addVariables: each list gets numbered 1 - 5
+    # addConstraint: apply exclusivity
+    for values in variables.values():
+        problem.addVariables(values, range(1,5+1))
+        problem.addConstraint(AllDifferentConstraint(), values)
 
-	# constraints: equal
-	for i in (
-		["brit"      , "red"    ],
-		["swede"     , "dogs"   ],
-		["dane"      , "tea"    ],
-		["green"     , "coffee" ],
-		["pallMall"  , "birds"  ],
-		["yellow"    , "dunhill"],
-		["blueMaster", "beer"   ],
-		["german"    , "prince" ]
-	):
-		problem.addConstraint(lambda a, b: a == b, i)
+    # constraints: equal
+    for i in (
+        ["brit"      , "red"    ],
+        ["swede"     , "dogs"   ],
+        ["dane"      , "tea"    ],
+        ["green"     , "coffee" ],
+        ["pallMall"  , "birds"  ],
+        ["yellow"    , "dunhill"],
+        ["blueMaster", "beer"   ],
+        ["german"    , "prince" ]
+    ):
+        problem.addConstraint(lambda a, b: a == b, i)
 
-	# constraints: next_to
-	for i in (
-		["blends"    , "cats"    ],
-		["horses"    , "dunhill" ],
-		["norwegian" , "blue"    ],
-		["blends"    , "water"   ]
-	):
-		problem.addConstraint(lambda a, b: a == b - 1 or a == b + 1, i)
+    # constraints: next_to
+    for i in (
+        ["blends"    , "cats"    ],
+        ["horses"    , "dunhill" ],
+        ["norwegian" , "blue"    ],
+        ["blends"    , "water"   ]
+    ):
+        problem.addConstraint(lambda a, b: a == b - 1 or a == b + 1, i)
 
-	# constraints: left_of, middle, first
-	problem.addConstraint(lambda a, b: a == b - 1, ["green"    , "white"])
-	problem.addConstraint(lambda a: a == 3       , ["milk"              ])
-	problem.addConstraint(lambda a: a == 1       , ["norwegian"         ])
+    # constraints: left_of, middle, first
+    problem.addConstraint(lambda a, b: a == b - 1, ["green"    , "white"])
+    problem.addConstraint(lambda a: a == 3       , ["milk"              ])
+    problem.addConstraint(lambda a: a == 1       , ["norwegian"         ])
 
-	# get solution
-	solution = problem.getSolution()
+    # get solution
+    solution = problem.getSolution()
 
-	# My convoluted formatting method
-	def order(key):
-		for position, array in enumerate(variables.values()):
-			if key in array:
-				return position
+    # My convoluted formatting method
+    def order(key):
+        for position, array in enumerate(variables.values()):
+            if key in array:
+                return position
 
-	ordered = [ 
-		sorted([ k for k,v in solution.items() if v == i], key=lambda x: order(x) ) 
-			for i in range(1, max(solution.values())+1)
-	]
+    ordered = [ 
+        sorted([ k for k,v in solution.items() if v == i], key=lambda x: order(x) ) 
+            for i in range(1, max(solution.values())+1)
+    ]
 
-	df = pd.DataFrame(
-		ordered,
-		index   = ["First" , "Second", "Third"  , "Fourth" , "Fifth"],
-		columns = ["Color:", "Race:" , "Smokes:", "Drinks:", "Pets:"]
-	).transpose()
+    df = pd.DataFrame(
+        ordered,
+        index   = ["First" , "Second", "Third"  , "Fourth" , "Fifth"],
+        columns = ["Color:", "Race:" , "Smokes:", "Drinks:", "Pets:"]
+    ).transpose()
 
-	print(df)
+    print(df)
 
 if __name__ == "__main__":
-	main()
+    main()
--- solutions/permutation.py
+++ solutions/permutation.py
@@ -1,55 +1,55 @@
 from itertools import permutations
 
 def main():
-	variables = {
-		'colors' : ['red'     , 'green'  , 'white' , 'yellow'    , 'blue'  ],
-		'races'  : ['brit'    , 'swede'  , 'dane'  , 'norwegian' , 'german'],
-		'drinks' : ['tea'     , 'coffee' , 'milk'  , 'beer'      , 'water' ],
-		'smokes' : ['pallMall', 'dunhill', 'blends', 'blueMaster', 'prince'],
-		'pets'   : ['dogs'    , 'birds'  , 'cats'  , 'horses'    , 'fish'  ]
-	}
+    variables = {
+        'colors' : ['red'     , 'green'  , 'white' , 'yellow'    , 'blue'  ],
+        'races'  : ['brit'    , 'swede'  , 'dane'  , 'norwegian' , 'german'],
+        'drinks' : ['tea'     , 'coffee' , 'milk'  , 'beer'      , 'water' ],
+        'smokes' : ['pallMall', 'dunhill', 'blends', 'blueMaster', 'prince'],
+        'pets'   : ['dogs'    , 'birds'  , 'cats'  , 'horses'    , 'fish'  ]
+    }
 
-	equals  = lambda a,b: True if a == b else False
-	next_to = lambda a,b: True if a == b - 1 or a == b +1 else False
-	left_of = lambda a,b: True if a == b - 1 else False
-						
-	perm = list(permutations(range(1, 5+1)))
-	for red, green, white, yellow, blue in perm:
-		if not left_of(green, white):
-			continue
-		for brit, swede, dane, norwegian, german in perm:
-			if not ( equals(brit, red) and next_to(norwegian, blue) ):
-				continue
-			for tea, coffee, milk, beer, water in perm:
-				if not ( equals(dane, tea) and equals(green, coffee) and \
-						 milk == 3         and norwegian == 1 ):
-					continue
-				for pallMall, dunhill, blends, blueMaster, prince in perm:
-					if not ( equals(yellow, dunhill) and equals(blueMaster, beer) and \
-							 equals(german, prince)  and next_to(blends, water) ):
-						continue
-					for dogs, birds, cats, horses, fish in perm:
-						if not ( equals(swede, dogs)   and equals(pallMall, birds) and \
-								 next_to(blends, cats) and next_to(horses, dunhill) ):
-							continue
+    equals  = lambda a,b: True if a == b else False
+    next_to = lambda a,b: True if a == b - 1 or a == b +1 else False
+    left_of = lambda a,b: True if a == b - 1 else False
+                        
+    perm = list(permutations(range(1, 5+1)))
+    for red, green, white, yellow, blue in perm:
+        if not left_of(green, white):
+            continue
+        for brit, swede, dane, norwegian, german in perm:
+            if not ( equals(brit, red) and next_to(norwegian, blue) ):
+                continue
+            for tea, coffee, milk, beer, water in perm:
+                if not ( equals(dane, tea) and equals(green, coffee) and \
+                         milk == 3         and norwegian == 1 ):
+                    continue
+                for pallMall, dunhill, blends, blueMaster, prince in perm:
+                    if not ( equals(yellow, dunhill) and equals(blueMaster, beer) and \
+                             equals(german, prince)  and next_to(blends, water) ):
+                        continue
+                    for dogs, birds, cats, horses, fish in perm:
+                        if not ( equals(swede, dogs)   and equals(pallMall, birds) and \
+                                 next_to(blends, cats) and next_to(horses, dunhill) ):
+                            continue
 
-						# combines old order strings to the newly ordered values then sorts them
-						# ('red'     , 3), ('green'  , 4), ('white' , 5), ('yellow'    , 1), ('blue'  , 2)
-						# ('brit'    , 3), ('swede'  , 5), ('dane'  , 2), ('norwegian' , 1), ('german', 4)
-						# ('tea'     , 2), ('coffee' , 4), ('milk'  , 3), ('beer'      , 5), ('water' , 1)
-						# ('pallMall', 3), ('dunhill', 1), ('blends', 2), ('blueMaster', 5), ('prince', 4)
-						# ('dogs'    , 5), ('birds'  , 3), ('cats'  , 1), ('horses'    , 2), ('fish'  , 4)
-						g = lambda x,y: [i for i,j in sorted(zip(x, y), key=lambda z: z[1])]
+                        # combines old order strings to the newly ordered values then sorts them
+                        # ('red'     , 3), ('green'  , 4), ('white' , 5), ('yellow'    , 1), ('blue'  , 2)
+                        # ('brit'    , 3), ('swede'  , 5), ('dane'  , 2), ('norwegian' , 1), ('german', 4)
+                        # ('tea'     , 2), ('coffee' , 4), ('milk'  , 3), ('beer'      , 5), ('water' , 1)
+                        # ('pallMall', 3), ('dunhill', 1), ('blends', 2), ('blueMaster', 5), ('prince', 4)
+                        # ('dogs'    , 5), ('birds'  , 3), ('cats'  , 1), ('horses'    , 2), ('fish'  , 4)
+                        g = lambda x,y: [i for i,j in sorted(zip(x, y), key=lambda z: z[1])]
 
-						# iterate through solved positions
-						for (k,v), new in zip(variables.items(), (
-							[red     , green  , white , yellow    , blue  ],
-							[brit    , swede  , dane  , norwegian , german],
-						    [tea     , coffee , milk  , beer      , water ],
-						    [pallMall, dunhill, blends, blueMaster, prince],
-						    [dogs    , birds  , cats  , horses    , fish  ]
-						)):
-							print("{:6s}: {:10s} {:10s} {:10s} {:10s} {:10s}".format(k, *g(v, new)))
+                        # iterate through solved positions
+                        for (k,v), new in zip(variables.items(), (
+                            [red     , green  , white , yellow    , blue  ],
+                            [brit    , swede  , dane  , norwegian , german],
+                            [tea     , coffee , milk  , beer      , water ],
+                            [pallMall, dunhill, blends, blueMaster, prince],
+                            [dogs    , birds  , cats  , horses    , fish  ]
+                        )):
+                            print("{:6s}: {:10s} {:10s} {:10s} {:10s} {:10s}".format(k, *g(v, new)))
 
 if __name__ == "__main__":
-	main()
+    main()
